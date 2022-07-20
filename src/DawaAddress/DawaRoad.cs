b1e18fadc85ed8a145784183b139bf987d8c5ed4
Rune Nielsen <runenielsen@runbox.com>
2022-07-20 07:44:11 +0200

    dawa road id is guid instead of string

--- src/DawaAddress/DawaRoad.cs
+++ src/DawaAddress/DawaRoad.cs
@@ -5,13 +5,13 @@ namespace DawaAddress;
 public record DawaRoad
 {
     [JsonPropertyName("id")]
-    public string Id { get; init; }
+    public Guid Id { get; init; }
     [JsonPropertyName("navn")]
     public string Name { get; init; }
     [JsonPropertyName("darstatus")]
     public string DarStatus { get; init; }
 
-    public DawaRoad(string id, string name, string darStatus)
+    public DawaRoad(Guid id, string name, string darStatus)
     {
         Id = id;
         Name = name;
