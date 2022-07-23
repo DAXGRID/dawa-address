c86038c41d8430ad5363436fac8aa743e93dbbd2
Rune Nielsen <runenielsen@runbox.com>
2022-07-23 11:11:07 +0200

    simplified to now all use same implementation of stream jsonline (#15)

--- src/DawaAddress/DawaClient.cs
+++ src/DawaAddress/DawaClient.cs
@@ -15,9 +15,9 @@ public class DawaClient
 
     public async Task<DawaTransaction> GetLatestTransactionAsync(CancellationToken cancellationToken = default)
     {
-        var transactionUrl = new Uri($"{_baseAddress}/senestetransaktion");
+        var uri = new Uri($"{_baseAddress}/senestetransaktion");
 
-        using var response = await _httpClient.GetAsync(transactionUrl, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+        using var response = await _httpClient.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
         using var stream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
 
         return await JsonSerializer.DeserializeAsync<DawaTransaction>(stream, options: null, cancellationToken).ConfigureAwait(false) ??
@@ -28,17 +28,10 @@ public class DawaClient
         ulong tId,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var accessAddressUrl = new Uri($"{_baseAddress}/udtraek?entitet=adgangsadresse&ndjson&txid={tId}");
-
-        using var response = await _httpClient.GetAsync(accessAddressUrl, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-        using var stream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
-        using var streamReader = new StreamReader(stream);
-
-        string? line = null;
-        while ((line = await streamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false)) is not null)
+        var uri = new Uri($"{_baseAddress}/udtraek?entitet=adgangsadresse&ndjson&txid={tId}");
+        await foreach (var dawaEntity in StreamDawaJsonLine<DawaAccessAddress>(uri, cancellationToken).ConfigureAwait(false))
         {
-            yield return JsonSerializer.Deserialize<DawaAccessAddress>(line)
-                ?? throw new DawaEmptyResultException("Received empty value from DAWA.");
+            yield return dawaEntity;
         }
     }
 
@@ -47,16 +40,9 @@ public class DawaClient
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
         var uri = new Uri($"{_baseAddress}/udtraek?entitet=adresse&ndjson&txid={tId}");
-
-        using var response = await _httpClient.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-        using var stream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
-        using var streamReader = new StreamReader(stream);
-
-        string? line = null;
-        while ((line = await streamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false)) is not null)
+        await foreach (var dawaEntity in StreamDawaJsonLine<DawaUnitAddress>(uri, cancellationToken).ConfigureAwait(false))
         {
-            yield return JsonSerializer.Deserialize<DawaUnitAddress>(line)
-                ?? throw new DawaEmptyResultException("Retrieved empty value from DAWA.");
+            yield return dawaEntity;
         }
     }
 
@@ -64,17 +50,10 @@ public class DawaClient
         ulong tId,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var postNumberUrl = new Uri($"{_baseAddress}/udtraek?entitet=navngivenvej&ndjson&txid={tId}");
-
-        using var response = await _httpClient.GetAsync(postNumberUrl, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-        using var stream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
-        using var streamReader = new StreamReader(stream);
-
-        string? line = null;
-        while ((line = await streamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false)) is not null)
+        var uri = new Uri($"{_baseAddress}/udtraek?entitet=navngivenvej&ndjson&txid={tId}");
+        await foreach (var dawaEntity in StreamDawaJsonLine<DawaRoad>(uri, cancellationToken).ConfigureAwait(false))
         {
-            yield return JsonSerializer.Deserialize<DawaRoad>(line)
-                ?? throw new DawaEmptyResultException("Retrieved empty value from DAWA.");
+            yield return dawaEntity;
         }
     }
 
@@ -82,17 +61,10 @@ public class DawaClient
         ulong tId,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var postNumberUrl = new Uri($"{_baseAddress}/udtraek?entitet=postnummer&ndjson&txid={tId}");
-
-        using var response = await _httpClient.GetAsync(postNumberUrl, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-        using var stream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
-        using var streamReader = new StreamReader(stream);
-
-        string? line = null;
-        while ((line = await streamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false)) is not null)
+        var uri = new Uri($"{_baseAddress}/udtraek?entitet=postnummer&ndjson&txid={tId}");
+        await foreach (var dawaEntity in StreamDawaJsonLine<DawaPostCode>(uri, cancellationToken).ConfigureAwait(false))
         {
-            yield return JsonSerializer.Deserialize<DawaPostCode>(line)
-                ?? throw new DawaEmptyResultException("Retrieved empty value from DAWA.");
+            yield return dawaEntity;
         }
     }
 
@@ -101,17 +73,10 @@ public class DawaClient
         ulong toTransactionId,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var url = new Uri(@$"{_baseAddress}/haendelser?entitet=adgangsadresse&ndjson&txidfra={fromTransactionId}&txidtil={toTransactionId}");
-
-        using var accessAddressResponse = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-        using var stream = await accessAddressResponse.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
-        using var streamReader = new StreamReader(stream);
-
-        string? line = null;
-        while ((line = await streamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false)) is not null)
+        var uri = new Uri(@$"{_baseAddress}/haendelser?entitet=adgangsadresse&ndjson&txidfra={fromTransactionId}&txidtil={toTransactionId}");
+        await foreach (var dawaEntity in StreamDawaJsonLine<DawaEntityChange<DawaAccessAddress>>(uri, cancellationToken).ConfigureAwait(false))
         {
-            yield return JsonSerializer.Deserialize<DawaEntityChange<DawaAccessAddress>>(line)
-                ?? throw new DawaEmptyResultException("Received empty value from DAWA.");
+            yield return dawaEntity;
         }
     }
 
@@ -120,17 +85,10 @@ public class DawaClient
         ulong toTransactionId,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var url = new Uri(@$"{_baseAddress}/haendelser?entitet=adresse&ndjson&txidfra={fromTransactionId}&txidtil={toTransactionId}");
-
-        using var accessAddressResponse = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-        using var stream = await accessAddressResponse.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
-        using var streamReader = new StreamReader(stream);
-
-        string? line = null;
-        while ((line = await streamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false)) is not null)
+        var uri = new Uri(@$"{_baseAddress}/haendelser?entitet=adresse&ndjson&txidfra={fromTransactionId}&txidtil={toTransactionId}");
+        await foreach (var dawaEntity in StreamDawaJsonLine<DawaEntityChange<DawaUnitAddress>>(uri, cancellationToken).ConfigureAwait(false))
         {
-            yield return JsonSerializer.Deserialize<DawaEntityChange<DawaUnitAddress>>(line)
-                ?? throw new DawaEmptyResultException("Received empty value from DAWA.");
+            yield return dawaEntity;
         }
     }
 
@@ -139,17 +97,10 @@ public class DawaClient
         ulong toTransactionId,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var url = new Uri(@$"{_baseAddress}/haendelser?entitet=navngivenvej&ndjson&txidfra={fromTransactionId}&txidtil={toTransactionId}");
-
-        using var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-        using var stream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
-        using var streamReader = new StreamReader(stream);
-
-        string? line = null;
-        while ((line = await streamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false)) is not null)
+        var uri = new Uri(@$"{_baseAddress}/haendelser?entitet=navngivenvej&ndjson&txidfra={fromTransactionId}&txidtil={toTransactionId}");
+        await foreach (var dawaEntity in StreamDawaJsonLine<DawaEntityChange<DawaRoad>>(uri, cancellationToken).ConfigureAwait(false))
         {
-            yield return JsonSerializer.Deserialize<DawaEntityChange<DawaRoad>>(line)
-                ?? throw new DawaEmptyResultException("Received empty value from DAWA.");
+            yield return dawaEntity;
         }
     }
 
@@ -158,16 +109,23 @@ public class DawaClient
         ulong toTransactionId,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var url = new Uri(@$"{_baseAddress}/haendelser?entitet=postnummer&ndjson&txidfra={fromTransactionId}&txidtil={toTransactionId}");
+        var uri = new Uri(@$"{_baseAddress}/haendelser?entitet=postnummer&ndjson&txidfra={fromTransactionId}&txidtil={toTransactionId}");
+        await foreach (var dawaEntity in StreamDawaJsonLine<DawaEntityChange<DawaPostCode>>(uri, cancellationToken).ConfigureAwait(false))
+        {
+            yield return dawaEntity;
+        }
+    }
 
-        using var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+    private async IAsyncEnumerable<T> StreamDawaJsonLine<T>(Uri uri, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        using var response = await _httpClient.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
         using var stream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
         using var streamReader = new StreamReader(stream);
 
         string? line = null;
         while ((line = await streamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false)) is not null)
         {
-            yield return JsonSerializer.Deserialize<DawaEntityChange<DawaPostCode>>(line)
+            yield return JsonSerializer.Deserialize<T>(line)
                 ?? throw new DawaEmptyResultException("Received empty value from DAWA.");
         }
     }
