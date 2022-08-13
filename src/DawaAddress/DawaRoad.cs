e5f91d05dfe08c0969a564750548781606f059f3
Rune Nielsen <runenielsen@runbox.com>
2022-08-13 14:15:36 +0200

    adds created and updated dates to road (#18)

--- src/DawaAddress/DawaRoad.cs
+++ src/DawaAddress/DawaRoad.cs
@@ -15,16 +15,32 @@ public record DawaRoad
 {
     [JsonPropertyName("id")]
     public Guid Id { get; init; }
+
     [JsonPropertyName("navn")]
     public string Name { get; init; }
+
     [JsonConverter(typeof(JsonStringEnumConverter<DawaRoadStatus>))]
     [JsonPropertyName("darstatus")]
     public DawaRoadStatus Status { get; init; }
 
-    public DawaRoad(Guid id, string name, DawaRoadStatus status)
+    [JsonPropertyName("oprettet")]
+    public DateTime Created { get; init; }
+
+    [JsonPropertyName("ændret")]
+    public DateTime Updated { get; init; }
+
+    [JsonConstructor]
+    public DawaRoad(
+        Guid id,
+        string name,
+        DawaRoadStatus status,
+        DateTime created,
+        DateTime updated)
     {
         Id = id;
         Name = name;
         Status = status;
+        Created = created;
+        Updated = updated;
     }
 }
